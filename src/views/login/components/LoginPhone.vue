15276453a802a7dbce1150d60eb787a5865fd50a
zhangheng <1515452760@qq.com>
2021-08-24 18:33:32 +0800

    fix(lint): lint

--- src/views/login/components/LoginPhone.vue
+++ src/views/login/components/LoginPhone.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="login-account">
-    <el-form label-width="70px" :rules="phoneRules" :model="phone"
-    ref="formRef">
+    <el-form label-width="70px" :rules="phoneRules" :model="phone" ref="formRef">
       <el-form-item label="手机号" prop="number">
         <el-input type="text" v-model="phone.number" />
       </el-form-item>
